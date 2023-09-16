056308f5f21a7dc048647e826702ecf21d43b235
priyanshusharman <priyanshuk1900@gmaul.com>
2023-09-16 07:22:57 +0530

    register join from backend

---
+++ loginauth/login.js
@@ -0,0 +1,40 @@
+let username=document.body.querySelector("#username").value;
+let state=document.body.querySelector("#state").value;
+let pincode=document.body.querySelector("#pincode").value;
+let email=document.body.querySelector("#email").value;
+let password=document.body.querySelector("#password").value;
+const update = () => {
+    username = document.body.querySelector("#username").value;
+    state = document.body.querySelector("#state").value;
+    pincode = document.body.querySelector("#pincode").value;
+    email = document.body.querySelector("#email").value;
+    password = document.body.querySelector("#password").value;
+}
+const createnewaccount = () => {
+    console.log("hello")
+    user = {
+        "name":username,
+        "email":email,
+        "state":state,
+        "pincode":pincode,
+        "password":password
+      }
+    let options = {
+        method: 'POST',
+        headers: {
+            'Content-Type':
+                'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(user)
+    }
+    let fetchRes = fetch(
+        "http://localhost:7000/api/auth/createuser",
+        options);
+    let result=fetchRes.then(res =>
+        res.json()).then(d => {
+            console.log(d)
+            return d;
+        })
+        console.log(result)
+
+}
